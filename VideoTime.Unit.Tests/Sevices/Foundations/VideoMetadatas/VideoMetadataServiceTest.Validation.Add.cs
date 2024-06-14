cf865b0d8560daf31df8cc875cad68652bae000b
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-06-14 21:34:49 +0500

    ShouldThrowValidationExceptionOnAddIfVideoMetadataIsInvalidDataAndLogItAsync->FAIL

--- VideoTime.Unit.Tests/Sevices/Foundations/VideoMetadatas/VideoMetadataServiceTest.Validation.Add.cs
+++ VideoTime.Unit.Tests/Sevices/Foundations/VideoMetadatas/VideoMetadataServiceTest.Validation.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Coalition Of Good-Hearted Engineers
 // Free To Use To Find Comfort And Peace
 //==================================================
+using FluentAssertions;
 using Moq;
 using VideoTime.Models.Exceptions;
 using VideoTime.Models.VideoMetadatas;
@@ -39,6 +40,61 @@ namespace VideoTime.Unit.Tests.Sevices.Foundations.VideoMetadatas
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfVideoMetadataIsInvalidDataAndLogItAsync(string invalidData)
+        {
+            // given
+            var invalidVideoMetadata = new VideoMetadata()
+            {
+                Title = invalidData
+            };
+
+            var invalidVideoMetadataException =
+                new InvalidVideoMetadataException(message: "Video metadata is invalid");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.Id),
+                values: "Id is required");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.Title),
+                values: "Text is required");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.BlobPath),
+                 values: "Text is required");
+
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.CreatedDate),
+                 values: "Data is required");
+            invalidVideoMetadataException.AddData(key: nameof(VideoMetadata.UpdatedDate),
+                 values: "Data is required");
+
+            var expectedVideoMetadataValidationExpected =
+                new VideoMetadataValidationException(
+                    message: "Video metadata Validation error occurred,fix the errors and try again",
+                innerException: invalidVideoMetadataException);
+
+            // when
+            ValueTask<VideoMetadata> addVideoMetadata =
+               this.videoMetadataService.AddVideoMetadataAsync(invalidVideoMetadata);
+
+            VideoMetadataValidationException actualvideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(addVideoMetadata.AsTask);
+
+            // then
+            actualvideoMetadataValidationException.Should().BeEquivalentTo(expectedVideoMetadataValidationExpected);
+
+            this.loggingBrokerMock.Verify(broker =>
+              broker.LogError(It.Is(SameExceptionAs(expectedVideoMetadataValidationExpected))),
+              Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+              broker.InsertVideoMetadataAsync(It.IsAny<VideoMetadata>()),
+              Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
---
+++ VideoTime/Models/Exceptions/InvalidVideoMetadataException.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+using Xeptions;
+
+namespace VideoTime.Models.Exceptions
+{
+    public class InvalidVideoMetadataException:Xeption
+    {
+        public InvalidVideoMetadataException(string message)
+          : base(message)
+        { }
+    }
+}
