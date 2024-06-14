4b3c69ccf2a02c6b50f09249c8ed5cc60e0b0d48
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-06-14 15:52:18 +0500

    BROKERS: CRUD VideoMetadata

--- VideoTime/Brokers/Storages/StorageBroker.VideoMetadata.cs
+++ VideoTime/Brokers/Storages/StorageBroker.VideoMetadata.cs
@@ -10,5 +10,19 @@ namespace VideoTime.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<VideoMetadata> VideoMetadatas { get; set; }
+        public async ValueTask<VideoMetadata> InsertVideoMetadataAsync(VideoMetadata videoMetadata) =>
+      await InsertAsync(videoMetadata);
+
+        public IQueryable<VideoMetadata> SelectAllVideoMetadatas() =>
+           SelectAll<VideoMetadata>();
+
+        public async ValueTask<VideoMetadata> SelectVideoMetadataByIdAsync(Guid videoMetadataId) =>
+            await SelectAsync<VideoMetadata>(videoMetadataId);
+
+        public async ValueTask<VideoMetadata> UpdateVideoMetadataAsync(VideoMetadata videoMetadata) =>
+          await UpdateAsync(videoMetadata);
+
+        public async ValueTask<VideoMetadata> DeleteVideoMetadataAsync(VideoMetadata videoMetadata) =>
+           await DeleteAsync(videoMetadata);
     }
 }
--- VideoTime/Brokers/Storages/StorageBroker.cs
+++ VideoTime/Brokers/Storages/StorageBroker.cs
@@ -16,6 +16,41 @@ namespace VideoTime.Brokers.Storages
             this.configuration = configuration;
             Database.Migrate();
         }
+        private async ValueTask<T> InsertAsync<T>(T @object) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Entry<T>(@object).State = EntityState.Added;
+            await broker.SaveChangesAsync();
+            return @object;
+        }
+        private IQueryable<T> SelectAll<T>() where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            return broker.Set<T>();
+        }
+        private async ValueTask<T> SelectAsync<T>(params object[] objectIds) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            return await broker.FindAsync<T>(objectIds);
+        }
+        private async ValueTask<T> UpdateAsync<T>(T @object) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Entry(@object).State = EntityState.Modified;
+            await broker.SaveChangesAsync();
+
+            return @object;
+        }
+        private async ValueTask<T> DeleteAsync<T>(T @object) where T : class
+        {
+            using var broker = new StorageBroker(this.configuration);
+            broker.Entry(@object).State = EntityState.Deleted;
+            await broker.SaveChangesAsync();
+
+            return @object;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString =
