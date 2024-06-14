f73df3620ffb0edd474a82e2ea2de7bb3cc9bac8
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-06-14 20:03:26 +0500

    BROKERS:LoggingBroker

---
+++ VideoTime/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,20 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+namespace VideoTime.Brokers.Loggings
+{
+    public class LoggingBroker:ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
+    }
+}
