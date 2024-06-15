2598f5449c60ac09c9698dd0371fff916ee1e7ea
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-06-15 22:59:37 +0500

    ShouldThrowValidationExceptionOnModifyIfVideoMetadataIsNullAndLogItAsync->FAIL

---
+++ VideoTime.Unit.Tests/Sevices/Foundations/VideoMetadatas/VideoMetadataServiceTest.Validation.Modify.cs
@@ -0,0 +1,47 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+using FluentAssertions;
+using Moq;
+using VideoTime.Models.Exceptions;
+using VideoTime.Models.VideoMetadatas;
+
+namespace VideoTime.Unit.Tests.Sevices.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTest
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfVideoMetadataIsNullAndLogItAsync()
+        {
+            // given
+            VideoMetadata nullVideoMetadata = null;
+            var nullVideoMetadataException = new NullVideoMetadataException("Video metadata is null");
+
+            var expectedVideoMetadataValidationException =
+                new VideoMetadataValidationException(
+                    message: "Video Metadata Validation Exception occurred, fix the errors and try again",
+                    innerException: nullVideoMetadataException);
+
+            // when
+            ValueTask<VideoMetadata> modifyVideoMetadataTask =
+                this.videoMetadataService.ModifyVideoMetadataAsync(nullVideoMetadata);
+
+            VideoMetadataValidationException actualVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(
+                    modifyVideoMetadataTask.AsTask);
+
+            // then
+            actualVideoMetadataValidationException.Should().BeEquivalentTo(
+                expectedVideoMetadataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedVideoMetadataValidationException))),
+                    Times.Once());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
