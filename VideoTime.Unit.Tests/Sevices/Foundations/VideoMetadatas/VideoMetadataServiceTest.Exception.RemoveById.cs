7d245179dc40a424bd612844692938716058f122
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-06-17 14:47:19 +0500

    ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync->PASS

--- VideoTime.Unit.Tests/Sevices/Foundations/VideoMetadatas/VideoMetadataServiceTest.Exception.RemoveById.cs
+++ VideoTime.Unit.Tests/Sevices/Foundations/VideoMetadatas/VideoMetadataServiceTest.Exception.RemoveById.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort And Peace
 //==================================================
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using VideoTime.Models.Exceptions;
 using VideoTime.Models.VideoMetadatas;
@@ -51,6 +52,51 @@ namespace VideoTime.Unit.Tests.Sevices.Foundations.VideoMetadatas
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedVideoMetadataDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedVideoMetadataException =
+                new LockedVideoMetadataException(
+                    message: "Video Metadata is locked, please try again",
+                    innerException: dbUpdateConcurrencyException);
+
+            var expectedVideoMetadataDependencyValidationException =
+                new VideoMetadataDependencyValidationException(
+                    message: "Video metadata Dependency validation error occured,fix the errors and try again",
+                    innerException: lockedVideoMetadataException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectVideoMetadataByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<VideoMetadata> removeVideoMetadataTask =
+                this.videoMetadataService.RemoveVideoMetadataByIdAsync(someId);
+
+            VideoMetadataDependencyValidationException actualVideoMetadataDependencyValidationException =
+                await Assert.ThrowsAsync<VideoMetadataDependencyValidationException>(
+                    removeVideoMetadataTask.AsTask);
+
+            // then
+            actualVideoMetadataDependencyValidationException.Should().BeEquivalentTo(
+                expectedVideoMetadataDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectVideoMetadataByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedVideoMetadataDependencyValidationException))),
                         Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
