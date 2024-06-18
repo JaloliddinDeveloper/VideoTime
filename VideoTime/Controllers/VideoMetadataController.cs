8e58b56dcef20bd28e2fc4cd5b8b60d7f2fe3509
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-06-18 18:23:29 +0500

    CONTROLLERS: Post VideoMetadata

---
+++ VideoTime/Controllers/VideoMetadataController.cs
@@ -0,0 +1,55 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using VideoTime.Models.Exceptions;
+using VideoTime.Models.VideoMetadatas;
+using VideoTime.Services.Foundations.VideoMetadatas;
+
+namespace VideoTime.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VideoMetadataController : RESTFulController
+    {
+        private readonly IVideoMetadataService videoMetadataService;
+
+        public VideoMetadataController(IVideoMetadataService videoMetadataService)
+        {
+            this.videoMetadataService = videoMetadataService;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<VideoMetadata>> PostVideoMetadataAsync(VideoMetadata videoMetadata)
+        {
+            try
+            {
+                VideoMetadata postedVideoMetadata =
+                await this.videoMetadataService.AddVideoMetadataAsync(videoMetadata);
+
+                return Created(postedVideoMetadata);
+            }
+            catch (VideoMetadataValidationException videoMetadataValidationException)
+            {
+                return BadRequest(videoMetadataValidationException.InnerException);
+            }
+            catch (VideoMetadataDependencyValidationException videoMetadataDependencyValidationException)
+                when (videoMetadataDependencyValidationException.InnerException is
+                    AlreadyExistVideoMetadataException)
+            {
+                return Conflict(videoMetadataDependencyValidationException.InnerException);
+            }
+            catch (VideoMetadataDependencyException videoMetadataDependencyException)
+            {
+                return InternalServerError(videoMetadataDependencyException);
+            }
+            catch (VideoMetadataDependencyServiceException videoMetadataDependencyServiceException)
+            {
+                return InternalServerError(videoMetadataDependencyServiceException);
+            }
+        }
+    }
+}
