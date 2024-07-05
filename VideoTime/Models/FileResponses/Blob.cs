62e383da6e6c643d226c81fe75c08ee931717951
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-07-05 20:57:03 +0500

    DATA: Add Blob and BlobContent Models

---
+++ VideoTime/Models/FileResponses/Blob.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+
+namespace VideoTime.Models.FileResponses
+{
+    public class Blob
+    {
+        public string Name { get; set; }
+        public string FileUrl { get; set; }
+        public string ContentType { get; set; }
+    }
+}
---
+++ VideoTime/Models/FileResponses/BlobContent.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//==================================================
+
+namespace VideoTime.Models.FileResponses
+{
+    public record BlobContent
+    {
+        public Stream Content { get; set; }
+        public string Name { get; set; }
+        public string ContentType { get; set; }
+    }
+}
